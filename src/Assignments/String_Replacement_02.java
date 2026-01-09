a6575391c4ea6247d33298558ecb30f9d43f1f7a
fasalat07 <fasal.at07@gmail.com>
2026-01-09 08:15:26 +0530

    commit20

--- src/Assignments/String_Replacement_02.java
+++ src/Assignments/String_Replacement_02.java
@@ -8,11 +8,10 @@ public class String_Replacement_02 {
     public static void main (String[] args){
 
         String input = "JavApRogr2m";
-
-        String output = input.replaceAll("[a-z]", "@")   // replace lowercase letters
-                .replaceAll("[0-9]", "&");  // replace digits
-
+        String output = input.replaceAll("[a-z]"," @").replaceAll("[0-9]", "&");
+        //Replaed lower case and numbers
         System.out.println(output);
+
 
     }
 }
